3779f74c476c6e31634ba6445ffb3f8610dd8abc
heesane <tees3359@gmail.com>
2024-11-05 15:06:51 +0900

    chore : publisher 초기세팅

---
+++ kafka_publisher/src/test/java/msa/heesane/kafka/KafkaPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.heesane.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
