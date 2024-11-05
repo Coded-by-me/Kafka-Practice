e5e5ae53820ce4f2cb35a8595221abc985aa16a4
heesane <tees3359@gmail.com>
2024-11-05 20:08:53 +0900

    chore : consumer Kafka Config

---
+++ kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConfig.java
@@ -0,0 +1,53 @@
+package msa.heesane.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String bootstrapServers;
+
+  @Value("${spring.kafka.consumer.group-id}")
+  private String groupId;
+
+  @Value("${spring.kafka.consumer.key-deserializer}")
+  private String keyDeserializer;
+
+  @Value("${spring.kafka.consumer.value-deserializer}")
+  private String valueDeserializer;
+
+  @Value("${spring.kafka.template.default-topic}")
+  private String topic;
+
+  @Bean
+  public ConsumerFactory<String, Object> consumerFactory(){
+
+    Map<String, Object> config = new HashMap<>();
+
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+
+    return new DefaultKafkaConsumerFactory<>(config);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
+
+}
