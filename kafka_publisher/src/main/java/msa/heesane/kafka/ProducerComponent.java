6c9662370b30bfa7b43ce59459610fed18db39e6
heesane <tees3359@gmail.com>
2024-11-05 20:10:49 +0900

    feat : Object로 String 형태로 Kafka로 전달

--- kafka_publisher/src/main/java/msa/heesane/kafka/ProducerComponent.java
+++ kafka_publisher/src/main/java/msa/heesane/kafka/ProducerComponent.java
@@ -1,5 +1,7 @@
 package msa.heesane.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,11 +12,14 @@ import org.springframework.stereotype.Component;
 public class ProducerComponent {
 
   private final KafkaTemplate<String, Object> kafkaTemplate;
+  private final ObjectMapper objectMapper;
 
   @Value("${spring.kafka.template.default-topic}")
   private String topic;
 
-  public void create(){
-    kafkaTemplate.send(topic,"testKey", "Hello, Kafka!");
+  public void create(String name, int age) throws JsonProcessingException {
+
+
+    kafkaTemplate.send(topic,"testKey", objectMapper.writeValueAsString(new TestDTO(name, age)));
   }
 }
