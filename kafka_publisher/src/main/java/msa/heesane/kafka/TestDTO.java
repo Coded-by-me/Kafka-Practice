d28e5dfc53e5eb3ca55f33a2219ff4278cd34545
heesane <tees3359@gmail.com>
2024-11-05 20:10:11 +0900

    feat : Test DTO

---
+++ kafka_publisher/src/main/java/msa/heesane/kafka/TestDTO.java
@@ -0,0 +1,11 @@
+package msa.heesane.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestDTO extends Object {
+    private String name;
+    private int age;
+}
