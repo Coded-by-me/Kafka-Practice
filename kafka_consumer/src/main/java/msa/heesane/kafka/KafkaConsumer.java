99ea97a147d6ace80fe591d575727e26a7748f44
heesane <tees3359@gmail.com>
2024-11-05 21:17:46 +0900

    feat : Consumer 데이터 수신 후 DTO로 Mapping

--- kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
+++ kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
@@ -1,14 +1,30 @@
 package msa.heesane.kafka;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaConsumer {
+
+  private final ObjectMapper objectMapper;
 
   @KafkaListener(topics = "my-topic", groupId = "my-group-id")
   public void consume(String message) {
     System.out.println("Received message: " + message);
+
+    TestDTO testDTO = null;
+
+    try{
+      testDTO = objectMapper.readValue(message, TestDTO.class);
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+
+    assert testDTO != null;
+    System.out.println("Name: " + testDTO.getName());
+    System.out.println("Age: " + testDTO.getAge());
   }
 }
