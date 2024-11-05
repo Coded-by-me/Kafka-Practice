0d61dd60c0ba5bb8b567718b260ecba7e55cb36c
heesane <tees3359@gmail.com>
2024-11-06 05:16:51 +0900

    feat: logging

--- kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
+++ kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
@@ -1,10 +1,17 @@
 package msa.heesane.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
@@ -12,19 +19,27 @@ public class KafkaConsumer {
   private final ObjectMapper objectMapper;
 
   @KafkaListener(topics = "my-topic", groupId = "my-group-id")
-  public void consume(String message) {
-    System.out.println("Received message: " + message);
+  public void consume(
+      String message,
+      @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+      @Header(KafkaHeaders.OFFSET) long offset,
+      @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp) {
 
     TestDTO testDTO = null;
 
-    try{
+    try {
       testDTO = objectMapper.readValue(message, TestDTO.class);
-    } catch(Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
     }
 
     assert testDTO != null;
-    System.out.println("Name: " + testDTO.getName());
-    System.out.println("Age: " + testDTO.getAge());
+
+    log.info("-------MESSAGE----------------");
+    log.info("Received Payload: {}", testDTO);
+    log.info("Received Partition: " + partition);
+    log.info("Received Offset: " + offset);
+    log.info("Received Timestamp: " + LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.of("Asia/Seoul")));
+    log.info("-------------------------------");
   }
 }
--- kafka_consumer/src/main/java/msa/heesane/kafka/TestDTO.java
+++ kafka_consumer/src/main/java/msa/heesane/kafka/TestDTO.java
@@ -2,9 +2,11 @@ package msa.heesane.kafka;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class TestDTO{
   private String name;
   private int age;
