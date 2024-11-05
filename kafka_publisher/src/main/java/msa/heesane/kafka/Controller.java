5cadd0ae8ec1468a5219bf21cfef09720e5e1cb4
heesane <tees3359@gmail.com>
2024-11-05 20:10:05 +0900

    feat : Test Controller에서 인자를 받아서, Kafka로 전달

--- kafka_publisher/src/main/java/msa/heesane/kafka/Controller.java
+++ kafka_publisher/src/main/java/msa/heesane/kafka/Controller.java
@@ -1,7 +1,9 @@
 package msa.heesane.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -10,9 +12,10 @@ public class Controller {
 
   private final ProducerComponent component;
 
-  @GetMapping("/test")
-  public void test(){
-    component.create();
+  @GetMapping("/test/{name}/{age}")
+  public void test(@PathVariable("name") String name, @PathVariable("age") int age)
+      throws JsonProcessingException {
+    component.create(name, age);
   }
 
 }
