329b86019c72bcc3576491d11a4e11302bfc6a3e
heesane <tees3359@gmail.com>
2024-11-10 02:34:29 +0900

    feat: Consumer through Avro

--- kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
+++ kafka_consumer/src/main/java/msa/heesane/kafka/KafkaConsumer.java
@@ -1,12 +1,11 @@
 package msa.heesane.kafka;
 
-import com.example.avro.TestDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import msa.heesane.avro_sample.TestDTO;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -17,30 +16,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
-  private final ObjectMapper objectMapper;
-
-  @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+  @KafkaListener(topics = "my-topic")
   public void consume(
-      String message,
+      TestDTO message,
       @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
       @Header(KafkaHeaders.OFFSET) long offset,
       @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp) {
 
-    TestDTO testDTO = null;
-
-    try {
-      testDTO = objectMapper.readValue(message, TestDTO.class);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    assert testDTO != null;
 
     log.info("-------MESSAGE----------------");
-    log.info("Received Payload: {}", testDTO);
+    log.info("Received Payload: {}", message);
     log.info("Received Partition: " + partition);
     log.info("Received Offset: " + offset);
-    log.info("Received Timestamp: " + LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.of("Asia/Seoul")));
+    log.info("Received Timestamp: " + LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp),
+        ZoneId.of("Asia/Seoul")));
     log.info("-------------------------------");
   }
 }
